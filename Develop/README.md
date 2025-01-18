765ee4a279e45d97f9e4de277f73ab2d9776606c
tlatimore <tlatimore0551@yahoo.com>
2025-01-18 13:22:58 -0500

    edited index.js and generateMarkdown.js

---
+++ Develop/README.md
@@ -0,0 +1,49 @@
+# README-Generator
+  ![Github license] (https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ## Description:
+  Generates a README of the user's project by asking a series of questions about the project such as "What command should be run for testing?", or "What information can you provide about the usage of your project?" so the user can provide information about the funcionality of their project.
+
+  ## Table of Contents:
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Contribution](#contribution)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  
+* [license](#license)
+
+
+  ## Installation
+
+  Run the following command(s) to install the necessary dependencies:
+  npm i
+
+  ## Usage
+
+  The usage of this project is for programmers that need to generate a file that provides information about the functionality of the project or a README
+
+  ## Contribution
+
+  Contact me for more regarding contribution
+
+  ## Tests
+
+  Run the following command(s) to run tests:
+
+  npm test
+
+  ## Questions
+
+  For any questions, please contact me using the information below:
+  GitHub: [Tlatimore](https://gitgub.com/Tlatimore)
+  Email: tlatimore0551@yahoo.com
+
+  ## License
+This project is licensed by the MIT license.
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
+import path from 'path';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Write a brief description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What information can you provide about the usage of your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What instructions can you provide for contributing to your project?',
+    },
+    {
+        type: 'input',
+        name: 'projectTests',
+        message: 'What command should be run for testing?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run for installation?',
+        default: 'npm i',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) =>
+    {
+     console.log('Generating Readme..');
+     writeToFile('README.md', generateMarkdown({...responses}));
+    });
+}
 
 // Function call to initialize app
 init();
